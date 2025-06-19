99f8efba2d3917e6f7581d4707544c2b9cf0051f
Jean <154575672+Veuxirdev@users.noreply.github.com>
2025-06-19 09:25:11 -0400

    Add operate function

---
+++ script.js
@@ -0,0 +1,29 @@
+let numberA = 0;
+let numberB = 0;
+let operator = "";
+
+const add = (a,b) => {
+    return a+b;
+}
+const subtract = (a,b) =>{
+    return a-b;
+}
+const multiply = (a,b) =>{
+    return a*b;
+}
+const divide = (a,b) =>{
+    return a/b;
+}
+
+const operate = (numA, opSymbol, numB) => {
+    switch(opSymbol){
+        case "+": 
+            return add(numA,numB);
+        case "-": 
+            return subtract(numA,numB);
+        case "X":
+            return multiply(numA,numB);
+        case "/":
+            return divide(numA,numB);
+    }
+}
